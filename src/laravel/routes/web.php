39ea9775f9d0e86397170d3f5b311b40851aa4ac
LassiqueiraNT <lassiqueira366@gmail.com>
2025-04-23 21:25:15 -0300

    update

--- src/laravel/routes/web.php
+++ src/laravel/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])
+    ->middleware('guest')
+    ->name('login');
